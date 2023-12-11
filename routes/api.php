da988d515c1a12db803b488a383ac8e0957daace
huangrongtong <huangrongtong@leniugame.com>
2023-12-11 15:19:17 +0800

    增加测试接口

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/search', function (Request $request) {
+    $header = $request->header();
+    return json_encode($header, JSON_UNESCAPED_UNICODE);
+});
